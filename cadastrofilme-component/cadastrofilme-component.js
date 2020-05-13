88e14db48fa4c222a568287f4e676c2fd5cf0ae8
Di-Go <alunodiego360@gmail.com>
2020-05-12 21:33:22 -0300

    Master - Criando e inserindo componente

--- cadastrofilme-component/cadastrofilme-component.js
+++ cadastrofilme-component/cadastrofilme-component.js
@@ -1,14 +1,12 @@
 const template = document.createElement('template');
-template.innerHTML = '<p> Tesntando Grande Texto </p>';
+template.innerHTML = '<p> Teste de Componente </p>';
 
-class BarraNavegacao extends HTMLElement {
+class CadastroFilme extends HTMLElement {
     constructor(){
         super();
 
         this.attachShadow({ mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.shadowRoot.querySelector()
-        
     }
 }
-window.customElements.define('barra-navegacao', BarraNavegacao)
+window.customElements.define('cadastro-filme', CadastroFilme)
--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
-
 hamburger.addEventListener("click", ()=>{
     navLinks.classList.toggle("open");
+});
+
+const main = document.getElementById("principal");
+const btnEntregas = document.getElementById("btnEntregas");
+btnEntregas.addEventListener("click", () => {
+    main.innerHTML = "<cadastro-filme></cadastro-filme>"
 });
