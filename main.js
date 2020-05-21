f7ffb1c56d67e85c0e8cf7cd4b567e5ccabee6cb
Di-Go <alunodiego360@gmail.com>
2020-05-20 23:08:20 -0300

    Inicio - Implementa barra de navegação Utilizando materialize

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
-hamburger.addEventListener("click", ()=>{
-    navLinks.classList.toggle("open");
-});
-
-const main = document.getElementById("principal");
-const btnEntregas = document.getElementById("btnEntregas");
-btnEntregas.addEventListener("click", () => {
-    main.innerHTML = "<cadastro-filme></cadastro-filme>"
+const elemsDropdown = document.querySelectorAll(".dropdown-trigger");
+const instanciaDropdown = M.Dropdown.init(elemsDropdown, {
+    coverTrigger: false
 });
