339d5e399a00fd836626df36d1b8f6c283fd04f4
Di-Go <alunodiego360@gmail.com>
2020-05-24 22:25:09 -0300

    MASTER - Configuração inicial do servidor

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Servidor executando em http://localhost:8081");
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const express = require('express');
+const appServer = express();
+
+// Rotas e Templates
+appServer.get('/', (request, response) => {
+    response.send("<h1>Servidor Executando<h1/>");
+});
+
+// Start Server
+appServer.listen(8081, (request, response) => {
+    console.log("Servidor executando em http://localhost:8081");
+});
