a72574077e1dcd43748aa83539546c3cd2134c8d
Di-Go <alunodiego360@gmail.com>
2020-06-04 21:05:17 -0300

    M001 - Reestruturação do projeto Adequando ao padrão de nomes utilizados na api

---
+++ configuracao.js
@@ -0,0 +1,32 @@
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const express = require('express');
+const servidor = express();
+
+//#region Configurações dos Módulos 
+    // Template Engine
+        servidor.engine("handlebars", handlebars({defaultLayout: 'main'}));
+        servidor.set('view engine','handlebars');
+    // Carregamento Estilos
+        servidor.use('/public', express.static('public'));
+//#endregion
+
+
+//#region Rotas e Templates
+    // Página Inicial
+        servidor.get("/", (requisicao, resposta) =>{
+            resposta.render('index');
+        });
+    // Cadastro de Categoria de Filmes
+        servidor.get("/categoria_filme", (requisicao, resposta) =>{
+            resposta.render('cadastro-categoria-filme');
+        });
+//#endregion
+
+//#region Configuração do Servidor
+    // Iniciar Servidor
+        const PORTA = 8081;
+        servidor.listen(PORTA, (requisicao, resposta) => {
+            console.log(`Servidor executando em http://localhost:${PORTA}`);
+        });
+//#endregion
